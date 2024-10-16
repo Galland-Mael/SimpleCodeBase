af2ad05dbc7b368ccf525f3b18752f33d42e9e23
mael-galland <maelgalland.71@gmail.com>
2024-10-17 00:37:38 +0200

    fix: migration id auto generated

--- src/main/resources/db/migration/R__1_insert_into_literary_genre.sql
+++ src/main/resources/db/migration/R__1_insert_into_literary_genre.sql
@@ -1,13 +1,13 @@
-INSERT INTO s_literary_type (id, code, label)
+INSERT INTO s_literary_type (code, label)
 VALUES
-    (1, 'FICTION', 'Fiction'),
-    (2, 'POETRY', 'Poetry'),
-    (3, 'DRAMA', 'Drama'),
-    (4, 'SCIENCE_FICTION', 'Science Fiction'),
-    (6, 'MYSTERY', 'Mystery'),
-    (7, 'HISTORICAL', 'Historical'),
-    (8, 'ROMANCE', 'Romance'),
-    (9, 'THRILLER', 'Thriller')
+    ( 'FICTION', 'Fiction'),
+    ( 'POETRY', 'Poetry'),
+    ( 'DRAMA', 'Drama'),
+    ( 'SCIENCE_FICTION', 'Science Fiction'),
+    ( 'MYSTERY', 'Mystery'),
+    ( 'HISTORICAL', 'Historical'),
+    ( 'ROMANCE', 'Romance'),
+    ( 'THRILLER', 'Thriller')
 ON CONFLICT (id) DO UPDATE
 SET code = excluded.code,
     label = excluded.label;
